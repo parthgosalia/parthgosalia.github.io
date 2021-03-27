74301576a588b01cdbd0b45585a3cf09fe6afdd0
Parth Gosalia <pgosalia44@gmail.com>
2021-03-27 13:08:18 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "19f98436038a49754a3fd39c6da223dc",
-"/": "19f98436038a49754a3fd39c6da223dc",
-"main.dart.js": "b562bff209b31ce29863165af59dd3df",
+"index.html": "0547a2be3949d50cdab391a4dc11ce41",
+"/": "0547a2be3949d50cdab391a4dc11ce41",
+"main.dart.js": "3dd42bfd50eaf839ed5b8bc21af76664",
 "manifest.json": "0db0f940990278494325e6a2ff585eb3",
 "version.json": "2ab2f389bc7f4b53682ae158488351ab"
 };
